f0840ab89dd89f1e85567718d9318cf58fb5b403
Samuel Tessaro <samueldesouzatessaro@gmail.com>
2026-04-07 10:54:24 -0300

    Firts

---
+++ _next/static/EekUkYRkzOnGmK85g-elo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-5b4b2c556912d0f1.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/EekUkYRkzOnGmK85g-elo/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[workspaceSlug]","\u002F[workspaceSlug]\u002F[projectSlug]","\u002F[workspaceSlug]\u002F[projectSlug]\u002F[docSlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
